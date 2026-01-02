a1bd7bdd8d2c9300d4d96dd77f725056095d41af
Ruby <93859705+MisterRuby@users.noreply.github.com>
2026-01-02 15:38:20 +0900

    chore(test-web): webmvc-test 의존성 추가

--- test-web/build.gradle.kts
+++ test-web/build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
 
     // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-webmvc-test")
     testImplementation("org.springframework.boot:spring-boot-starter-security-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
